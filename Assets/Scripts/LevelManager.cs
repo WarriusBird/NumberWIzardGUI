442959b0f85cee1ebede37937b8d1d38727e9846
WarriusBird <nbgibson@crimson.ua.edu>
2017-06-01 00:25:09 -0400

    Added text and start button

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for " + name);
+    }
+}
