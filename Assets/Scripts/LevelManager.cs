2dec9095994c8d001746ab22945097d54ddc2008
WarriusBird <nbgibson@crimson.ua.edu>
2017-06-04 12:40:38 -0400

    Completed game
    Restructured game logic and progression. Added cosmetic touches. Finished game and added a HTML build into the gamedata folder.

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,5 +6,12 @@ public class LevelManager : MonoBehaviour {
 	public void LoadLevel(string name)
     {
         Debug.Log("Level load requested for " + name);
+        Application.LoadLevel(name);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Exiting game");
+        Application.Quit();
     }
 }
